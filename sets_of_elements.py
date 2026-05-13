2db132e4e5a06770b7f16b59a9bda7b39d4d9ef3
Zhelyazko Dedov <109466030+zhel13@users.noreply.github.com>
2026-05-14 00:30:32 +0300

    cleaned code

--- sets_of_elements.py
+++ sets_of_elements.py
@@ -1,12 +1,7 @@
 set1_range, set2_range = map(int, input().split())
 
-set1 = set()
-set2 = set()
-
-for _ in range(set1_range):
-    set1.add(int(input()))
-for _ in range(set2_range):
-    set2.add(int(input()))
+set1 = set(int(input()) for _ in range(set1_range))
+set2 = set(int(input()) for _ in range(set2_range))
 
 result = set1&set2
 print(*result, sep='\n')
